b9fd4a8d1b0dc1e8adf559c5b0f725881ba36021
Maximilian Schön <maxleonschoen@gmail.com>
2021-05-26 21:23:38 +0200

    added more

--- src/Game.java
+++ src/Game.java
@@ -1,19 +1,19 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Game {
 	
 	int columns = 7;
 	int rows = 6;
 	String empty = " ";
-	String[][] board = new String[columns][rows]{new String[]{}"| 1 | 2 | 3 | 4 | 5 | 6 | 7 |"
-										+ "|   |   |   |   |   |   |   |"
-										+ "|   |   |   |   |   |   |   |"
-										+ "|   |   |   |   |   |   |   |"
-										+ "|   |   |   |   |   |   |   |"
-										+ "|   |   |   |   |   |   |   |"
-										+ "|   |   |   |   |   |   |   |"};
-	int[] spalteFuellstand = new int[7];
+	String[][] board = new String[columns][rows]{"| 1 | 2 | 3 | 4 | 5 | 6 | 7 |"
+												+ null + null + null + null + null + null + null
+												+ null + null + null + null + null + null + null
+												+ null + null + null + null + null + null + null
+												+ null + null + null + null + null + null + null
+												+ null + null + null + null + null + null + null
+												+ null + null + null + null + null + null + null};
 	int player = 1;
 	Map<Integer, String> playerSymbols = new HashMap<Integer, String>();
 	playerSymbols.put(1, "○");
@@ -21,43 +21,52 @@ public class Game {
 	
 	void printBoard() {
 		System.out.println("| 1 | 2 | 3 | 4 | 5 | 6 | 7 |");
-		for (int row = 0, int column = 0; row < board[0].length - 2 && column < board.length; row++, column++) {
+		for (int row = 1, column = 0; row < board[0].length - 2 && column < board.length; row++, column++) {
 			System.out.println(String.format("| %c | %c | %c | %c | %c | %c | %c |", board[column][row]));
 		}
-				// end of if else if (SpielFeld[ZeilenCounter][SpaltenCounter] == 1) { System.out.print("| " + "X" + " "); } 
-				// end of if-else else if (SpielFeld[ZeilenCounter][SpaltenCounter] == 2) { System.out.print("| " + "O" + " "); }
-				// end of if-else } // end of for System.out.print("|"); System.out.println(); System.out.println("|___|___|___|___|___|___|___|"); } 
-				// end of for System.out.println(); } 
-				// end of private static void SpielFeldAusgeben private static void Spielerwechsel() {
-				// Spielerwechsel if (SpielerEins == true) {
-				SpielerEins = false; } // end of if else {
-			SpielerEins = true; }
-		// end of if-else } 
-		// end of private static void Spielerwechsel private static void Spalteneingabe() { 
-		// Spalteneingabe durch Spieler eins oder zwei try{ int Spieler; Spalte = 0; String EingabeA;
-		if (SpielerEins == true) {
-			Object EingabeA = Input.readString("Spieler 1: Bitte Spaltennummer angeben: "); }
-		// end of if else { EingabeA = Input.readString("Spieler 2: Bitte Spaltennummer angeben: "); } 
-		// end of if-else // Prüfen der Eingabe auf einen Wert zwischen 1 und 7 Spalte = Integer.parseInt(EingabeA); }
-		  "catch"(Exception ) ; {
-			//System.out.println("Bitte Zahl zwischen 1 und 7 eingeben"); }
-			if (Spalte < 1 || Spalte > 7) { 
-				System.out.println(); System.out.println("Achtung! Die eingegebene Spaltennummer muss zwischen 1 und 7 liegen!");
-				Spalteneingabe(); }
-			else if (spalteFuellstand[Spalte - 1] > 5) { 
-				System.out.println("Spalte voll!!!!"); Spalteneingabe(); } 
-			else { spalteFuellstand[Spalte - 1]++; }
-			private static void Spalteneingabe() {
-		
+		System.out.println();
 	}
-			private static void Spielzug() {
+	
+	void changePlayers() {
+		switch(player) {
+			case 1 : player++; break;
+			case 2 : player--;
+		}
+	}
+	
+	void putIn() {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println(String.format("Spieler %d: Bitte Spaltennummer angeben.", player));
+		int column = keyboard.nextInt();
+		if (1 <= column && column >= 7 && !isFull(column)) {
+			level++;
+		} else if (isFull(column)) {
+			System.out.println("Die Spalte ist voll!");
+		} else if (column < 1) {
+			System.out.println("Achtung, die eingegebene Spaltennummer darf 1 nicht unterschreiten!");
+		} else if (column > 7) {
+			System.out.println("Achtung, die eingegebene Spaltennummer darf 7 nicht überschreiten!");
+		}
+	}
+	
+	boolean isFull(int column) {
+		int level;
+		for (column : board.length) {
+			for (int row = 1 : board[column].length) {
+				if (board[column][row] != null) {
+					level++;
+					if (level == 7) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+	
+	void Spielzug() {
 				if (SpielerEins == true) {
-					SpielFeld[6 - spalteFuellstand[Spalte - 1]][Spalte - 1] = 1; }
-				int s;
-				// end of if else { SpielFeld[6 - spalteFuellstand[Spalte - 1]][Spalte - 1] = 2; }
-				// end of if-else }// end of private static void Spielzug private static void Gewinnbedingung() { boolean a=false; int s; 
-				if (SpielerEins == true) { s=1; }
-				else { s=2; }
+					SpielFeld[6 - spalteFuellstand[Spalte - 1]][Spalte - 1] = 1;}
 				boolean a;
 				for(int i=0;i<=2;i++) for(int j=0;j<=6;j++) {
 					if(SpielFeld [i] [j]==s && SpielFeld [i+1] [j]==s && SpielFeld [i+2] [j] ==s && SpielFeld [i+3] [j] ==s){a=true;}}
--- src/Input.java
+++ src/Input.java
@@ -1,25 +1,3 @@
-/**
- * 
- */
-package game;
-
-/**
- * @author tanja
- *
- */
 public class Input {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public static Object readString(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	
 }
