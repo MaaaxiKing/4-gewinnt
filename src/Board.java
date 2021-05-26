e289eb844867087527f799643dcfda572011917d
Maximilian Schön <maxleonschoen@gmail.com>
2021-05-26 22:07:34 +0200

    prepared

--- src/Board.java
+++ src/Board.java
@@ -6,8 +6,21 @@ public class Board {
 	String[][] board = new String[columns][rows];
 	
 	Board() {
-		for (int column=0; column < column; column++) {//wieso nicht ohne = 0?
+		for (int column = 0; column < columns; column++) {//wieso nicht ohne = 0?
 			board[column][0] = String.format("| %d ", column);
 		}
+	}
+	
+	boolean isFull(int column) {
+		int level;
+		for (int row = 1 : board[column]) {
+			if (board[column][row] != null) {
+				level++;
+				if (level == 7) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
--- src/Game.java
+++ src/Game.java
@@ -44,45 +44,4 @@ public class Game {
 			System.out.println("Achtung, die eingegebene Spaltennummer darf 7 nicht überschreiten!");
 		}
 	}
-	
-	boolean isFull(int column) {
-		int level;
-		for (column : board.length) {
-			for (int row = 1 : board[column].length) {
-				if (board[column][row] != null) {
-					level++;
-					if (level == 7) {
-						return true;
-					}
-				}
-			}
-		}
-		return false;
-	}
-	
-	/*void Spielzug() {
-				if (SpielerEins == true) {
-					SpielFeld[6 - spalteFuellstand[Spalte - 1]][Spalte - 1] = 1;}
-				boolean a;
-				for(int i=0;i<=2;i++) for(int j=0;j<=6;j++) {
-					if(SpielFeld [i] [j]==s && SpielFeld [i+1] [j]==s && SpielFeld [i+2] [j] ==s && SpielFeld [i+3] [j] ==s){a=true;}}
-				for(int i=0;i<=5;i++) 
-					for(int j=0;j<=3;j++) {if(SpielFeld [i] [j]==s && SpielFeld [i] [j+1]==s && SpielFeld [i] [j+2] ==s && SpielFeld [i] [j+3] ==s) {a=true;} } 
-				for(int i=0;i<=2;i++) for(int j=0;j<=3;j++) {if(SpielFeld [i] [j]==s && SpielFeld [i+1] [j+1]==s && SpielFeld [i+2] [j+2] ==s && SpielFeld [i+3] [j+3] ==s) {a=true;} }
-				if (a==true) { System.out.println ("Gewonnen hat Spieler " + s ); String EingabeEnde = (String) Input.readString("Weiterspielen (0) oder Beenden (1)");
-				int Eingabe = Integer.parseInt(EingabeEnde) ; 
-				if (Eingabe== 1) { System.exit(0);}
-				else if (Eingabe==0) {
-					SpielFeldAusgeben(); } } }
-			public static void () {
-				while (true) { Spalteneingabe(); Spielzug(); SpielFeldAusgeben(); Gewinnbedingung(); Spielerwechsel(); 
-				}
-				}
-	private void Gewinnbedingung() {
-		
-	}
-
-	private void Spielerwechsel() {
-		
-	}*/
 }
